08e64962a205b8c86a3a67dcefbd1aa861606206
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-03-03 01:58:25 +0000

    Added code_gen.py and main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: March 3, 2025
+# DESCRIPTION HERE
+
+
+def main():
+    # Greetings
+
+    # INPUT
+    # Ask User for amount of sides in the base
+    # Ask User for type of shape (prism/conal)
+
+    # OUTPUT
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
