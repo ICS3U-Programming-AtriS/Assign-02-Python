52a7c2c78158d5cd4213b2e627b2eb7ee94d5340
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-03-07 23:49:32 +0000

    More comments added

--- output_handler.py
+++ output_handler.py
@@ -3,11 +3,13 @@
 # Date: March 3, 2025
 # This module is in charge of creating the output file that contains the code
 
+# File path to the file that will get the generated code
 output_file_path = (
     "/home/vscode/ICS3U/Assign/Assign-02/Assign-02-Python/OUTPUT/output_code.py"
 )
 
 
+# Function that adds the code to the output file
 def addCodeToFile(code: str):
     with open(output_file_path, "r+") as file:
         # Delete all contents of file
